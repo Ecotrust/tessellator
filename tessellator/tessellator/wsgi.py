6454109f563af6f579eb19547e9daa4ef92980dc
Seth Hill <seth@pointnineseven.com>
2014-12-01 10:53:49 -0800

    Typo

--- tessellator/tessellator/wsgi.py
+++ tessellator/tessellator/wsgi.py
@@ -7,7 +7,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tessellator.settings")
 
-activate_this = os.expandvars('$HOME/env/tessellator/bin/activate_this.py')
+activate_this = os.path.expandvars('$HOME/env/tessellator/bin/activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
