c772a507e7d5051e1bc664b99d21844c1f4ace80
Seth Hill <seth@pointnineseven.com>
2014-12-01 10:51:52 -0800

    Be a bit generic about location of virtual environment.

--- tessellator/tessellator/wsgi.py
+++ tessellator/tessellator/wsgi.py
@@ -2,13 +2,13 @@
 WSGI config for tessellator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tessellator.settings")
+
+activate_this = os.expandvars('$HOME/env/tessellator/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
